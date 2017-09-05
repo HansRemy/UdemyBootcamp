04208a34b19817028e3ca4bf02b519ee9762fd55
Hans Remy <hansremy76@gmail.com>
2017-09-05 23:40:29 +0000

    after Yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -6,7 +6,9 @@ var express     =   require("express"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
     User = require("./models/user"),
-    seedDB      = require("./seeds")
+    seedDB      = require("./seeds"),
+    methodOverride = require("method-override"),
+    flash = require("connect-flash")
     
 var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
@@ -18,8 +20,9 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
-
+app.use(methodOverride("_method"));
 // seedDB();
+app.use(flash());
 // passport config
 app.use(require("express-session")({
     secret:"ik ben de beste",
@@ -28,12 +31,15 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
+var middleware = require("../middleware");
 
 
 router.get("/", function(req,res){
@@ -13,7 +14,7 @@ router.get("/", function(req,res){
     });
 });
 
-router.get("/new",isLoggedIn, function(req,res){
+router.get("/new",middleware.isLoggedIn, function(req,res){
     res.render("campgrounds/new");
 });
 
@@ -30,15 +31,16 @@ router.get("/:id",function(req,res){
 });
 
 // create
-router.post("/",isLoggedIn,function(req,res){
+router.post("/",middleware.isLoggedIn,function(req,res){
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {name: name,price: price, image: image, description: desc, author: author};
     Campground.create(newCampground,function(err, newlyCreated){
        if(err){
            console.log(err);
@@ -49,11 +51,35 @@ router.post("/",isLoggedIn,function(req,res){
     });
 });
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+// edit
+router.get("/:id/edit",middleware.checkCampgroundOwnership, function (req, res) {
+        Campground.findById(req.params.id, function(err, foundCampground){
+                    res.render("campgrounds/edit",{campground: foundCampground}); 
+        });      
+});
+
+// update
+router.put("/:id",middleware.checkCampgroundOwnership ,  function(req,res){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// destroy
+router.delete('/:id',middleware.checkCampgroundOwnership, function(req,res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+
 
 module.exports = router;
